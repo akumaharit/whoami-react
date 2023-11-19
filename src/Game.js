3413bf81d01f28174842e1e4885a6440949043a1
akumaharit <82520836+akumaharit@users.noreply.github.com>
2023-11-19 20:38:37 +0700

    Attempting to update github page

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { initializeApp } from 'firebase/app';
-import { getFirestore, doc, getDoc} from 'firebase/firestore';
 
 function Game(){
     return (
         <div id='game'>
-            'testing connection to Game.js and Firestore'<br/> 
+            'ทดสอบอัปเดท Github Page... <br/>testing connection to Game.js and Firestore'<br/> 
             The output is as follow:
         </div>
     );
